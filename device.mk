be69c1ac728afad3a0d750db9b18bf2648b1a4f5
Alexander Koskovich <akoskovich@pm.me>
2022-03-06 02:11:47 -0700

    pro1x: Update to Boot Control HAL v1.2
    Change-Id: Ib5d06c9b0c7e2ed000eb03a9bf18e4d78cc8740d

--- device.mk
+++ device.mk
@@ -96,9 +96,9 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Boot Control
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot@1.2-impl-qti \
+    android.hardware.boot@1.2-impl-qti.recovery \
+    android.hardware.boot@1.2-service
 
 # Camera
 PRODUCT_PACKAGES += \
