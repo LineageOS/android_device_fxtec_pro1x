9b4bac504ff2e9b70c8c4f9bb298461f707480c3
Sean McCreary <mccreary@mcwest.org>
2022-03-27 20:30:16 -0600

    pro1x: Add KeyDisabler
    Include KeyDisabler to allow the user to disable the navigation
    bar in Settings
    
    Co-authored-by: Paul Keith <javelinanddart@gmail.com>
    Change-Id: I032d552a04e9f5a5ef063ae9e796f3cb6bfbd3e4

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -73,7 +73,8 @@ TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
-    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    $(DEVICE_PATH)/framework_manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
 
---
+++ touch/service.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2019,2021-2022 The LineageOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#define LOG_TAG "vendor.lineage.touch@1.0-service.pro1"
+
+#include <android-base/logging.h>
+#include <hidl/HidlTransportSupport.h>
+
+#include "KeyDisabler.h"
+
+using android::OK;
+using android::sp;
+using android::hardware::configureRpcThreadpool;
+using android::hardware::joinRpcThreadpool;
+
+using ::vendor::lineage::touch::V1_0::IKeyDisabler;
+using ::vendor::lineage::touch::V1_0::implementation::KeyDisabler;
+
+int main() {
+    sp<IKeyDisabler> key_disabler = new KeyDisabler();
+
+    configureRpcThreadpool(1, true /*callerWillJoin*/);
+
+    if (key_disabler->registerAsService() != OK) {
+        LOG(ERROR) << "Cannot register keydisabler HAL service.";
+        return 1;
+    }
+
+    LOG(INFO) << "Touch HAL service is ready.";
+    joinRpcThreadpool();
+    // Should not pass this line
+
+    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    return 1;
+}
