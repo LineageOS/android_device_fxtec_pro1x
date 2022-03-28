d53ca54e182d19360087fc68b1fbcfdaf4eafc1d
Sean McCreary <mccreary@mcwest.org>
2022-03-27 20:30:16 -0600

    pro1x: Add KeyDisabler
    Include KeyDisabler to allow the user to disable the navigation
    bar in Settings
    
    Co-authored-by: Paul Keith <javelinanddart@gmail.com>
    Change-Id: I032d552a04e9f5a5ef063ae9e796f3cb6bfbd3e4

--- devicesettings/src/org/lineageos/settings/device/BootCompletedReceiver.java
+++ devicesettings/src/org/lineageos/settings/device/BootCompletedReceiver.java
@@ -11,15 +11,24 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import lineageos.providers.LineageSettings;
+
 import org.lineageos.settings.device.doze.DozeUtils;
 import org.lineageos.settings.device.keyboard.KeyboardUtils;
 import org.lineageos.settings.device.touchscreen.TouchscreenUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
+    private static final String FORCE_SHOW_NAVBAR_UPDATED = "force_show_navbar_updated";
 
     @Override
     public void onReceive(Context context, Intent intent) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        if (!prefs.contains(FORCE_SHOW_NAVBAR_UPDATED)) {
+            LineageSettings.System.putInt(context.getContentResolver(),
+                    LineageSettings.System.FORCE_SHOW_NAVBAR, 1);
+            prefs.edit().putBoolean(FORCE_SHOW_NAVBAR_UPDATED, true).commit();
+        }
 
         DozeUtils.checkDozeService(context);
         KeyboardUtils.setKeyboardKeymap(prefs);
