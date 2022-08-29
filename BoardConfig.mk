b657b9688e0317312eabd77f28e0dd5b5dda8fe0
Edwin Moquete <edwinmmoquete@gmail.com>
2022-08-29 01:37:22 +0000

    pro1x: Switch to legacy sepolicy_vndr
    Change-Id: I13229c3057bf32c81b0fdace075f0791cb8ad380

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -193,7 +193,7 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := 2022-06-01
 
 # SEPolicy
-include device/qcom/sepolicy_vndr/SEPolicy.mk
+include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include $(DEVICE_PATH)/sepolicy/SEPolicy.mk
 
 # Verified Boot
