ae65664ff0f367e6723e3f0883d0c83fa68aa168
Georg Veichtlbauer <georg@vware.at>
2023-02-06 09:15:14 +0100

    pro1x: Enable ZRAM with 30% size
    As we expect vendor updates, separate setup from vendor supplied
    scripts / fstab.
    
    Change-Id: I4f30f2badd7aaad6e94c5e9a77341b358a996828

--- device.mk
+++ device.mk
@@ -229,7 +229,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.default \
     fstab.default.ramdisk \
+    fstab.zram \
     init.class_main.sh \
+    init.lineage.rc \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
