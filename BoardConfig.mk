155236315416280764dea236c1919894356b37b2
Bruno Martins <bgcngm@gmail.com>
2023-04-22 09:48:45 +0100

    pro1x: Drop BUILD_BROKEN_DUP_RULES
    Change-Id: I092fb9e466265c386d9056dad8776f7a9aba83d5

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,10 +1,9 @@
 #
-# Copyright (C) 2022 The LineageOS Project
+# Copyright (C) 2022-2023 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
-BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 
 DEVICE_PATH := device/fxtec/pro1x
