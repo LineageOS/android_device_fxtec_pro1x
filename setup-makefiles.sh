2ead969a95056e4938d991d28e3ce5532b4e5318
Aaron Kling <webgeek1234@gmail.com>
2023-08-29 15:57:49 -0500

    pro1x: Add vendor soong imports to extract
    Change-Id: I7b290a9412bd11bb5577ce634a0291ffca40b283

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -26,6 +26,18 @@ if [ ! -f "${HELPER}" ]; then
 fi
 source "${HELPER}"
 
+function vendor_imports() {
+    cat <<EOF >>"$1"
+		"hardware/qcom-caf/common/libqti-perfd-client",
+		"hardware/qcom-caf/sm8250",
+		"hardware/qcom-caf/wlan",
+		"vendor/qcom/opensource/display",
+		"vendor/qcom/opensource/commonsys/display",
+		"vendor/qcom/opensource/commonsys-intf/display",
+		"vendor/qcom/opensource/dataservices",
+EOF
+}
+
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 
