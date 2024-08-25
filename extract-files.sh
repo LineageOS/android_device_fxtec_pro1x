5b31b165a2f151a7a99a2a044773438e7ea84d42
Michael Bestas <mkbestas@lineageos.org>
2024-08-25 21:46:46 +0300

    pro1x: Patch libwfdservice to resolve duplicate dependencies
    Fixes:
    error: module "libwfdservice" variant
    "android_arm_armv8-a_cortex-a76_static": depends on multiple
    versions of the same aidl_interface:
    android.media.audio.common.types-V2-cpp-source,
    android.media.audio.common.types-V3-cpp-source
    
    Change-Id: I8086a80fcc238d6b03d16640ee2c4e134129bbab

--- extract-files.sh
+++ extract-files.sh
@@ -85,6 +85,10 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
             ;;
+        system_ext/lib/libwfdservice.so)
+            [ "$2" = "" ] && return 0
+            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
+            ;;
         *)
             return 1
             ;;
