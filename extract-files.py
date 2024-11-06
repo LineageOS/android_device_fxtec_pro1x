f5d468efac2670f10e460168f4e586c90e4281c2
LuK1337 <priv.luk@gmail.com>
2024-11-06 11:56:59 +0100

    pro1x: Use common lib_fixups
    Change-Id: I862e547f3375184729eb1a909bb7d6f676d45dfe

--- extract-files.py
+++ extract-files.py
@@ -9,11 +9,8 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove_arch_suffix,
-    lib_fixup_vendorcompat,
+    lib_fixups,
     lib_fixups_user_type,
-    libs_clang_rt_ubsan,
-    libs_proto_3_9_1,
 )
 from extract_utils.main import (
     ExtractUtils,
@@ -31,20 +28,18 @@ namespace_imports = [
     'vendor/qcom/opensource/dataservices',
 ]
 
-libs_add_vendor_suffix = (
-    'com.qualcomm.qti.dpm.api@1.0',
-    'libmmosal',
-    'vendor.qti.hardware.wifidisplaysession@1.0',
-    'vendor.qti.imsrtpservice@3.0',
-)
 
 def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
     return f'{lib}_{partition}' if partition == 'vendor' else None
 
 lib_fixups: lib_fixups_user_type = {
-    libs_clang_rt_ubsan: lib_fixup_remove_arch_suffix,
-    libs_proto_3_9_1: lib_fixup_vendorcompat,
-    libs_add_vendor_suffix: lib_fixup_vendor_suffix,
+    **lib_fixups,
+    (
+        'com.qualcomm.qti.dpm.api@1.0',
+        'libmmosal',
+        'vendor.qti.hardware.wifidisplaysession@1.0',
+        'vendor.qti.imsrtpservice@3.0',
+    ): lib_fixup_vendor_suffix,
 }
 
 blob_fixups: blob_fixups_user_type = {
