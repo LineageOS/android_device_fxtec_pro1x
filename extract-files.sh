8e346344a0f1ed7a387d3fcab579f7d6d939b31d
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-26 16:07:18 -0500

    pro1x: Use patchelf 0.18 everywhere
    * Unpin all the things while we're here, we patch at extract time.
    
    Change-Id: I970d0f9efc7582be9371ec225880d259e413bf07

--- extract-files.sh
+++ extract-files.sh
@@ -61,7 +61,7 @@ function blob_fixup() {
     case "${1}" in
         system_ext/lib64/lib-imsvideocodec.so)
             [ "$2" = "" ] && return 0
-            grep -q libgui_shim.so "${2}" || "${PATCHELF}" --add-needed libgui_shim.so "${2}"
+            grep -q libgui_shim.so "${2}" || "${PATCHELF_0_18}" --add-needed libgui_shim.so "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
             [ "$2" = "" ] && return 0
@@ -69,23 +69,23 @@ function blob_fixup() {
             ;;
         vendor/lib64/hw/camera.qcom.so)
             [ "$2" = "" ] && return 0
-            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
+            grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF_0_18}" --add-needed libcamera_metadata_shim.so "${2}"
             ;;
         vendor/lib64/libwvhidl.so | vendor/lib64/mediadrm/libwvdrmengine.so)
             [ "$2" = "" ] && return 0
-            grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
+            grep -q libcrypto_shim.so "${2}" || "${PATCHELF_0_18}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
         system_ext/lib*/libwfdmmsrc_system.so)
             [ "$2" = "" ] && return 0
-            grep -q "libgui_shim.so" "${2}" || "${PATCHELF}" --add-needed "libgui_shim.so" "${2}"
+            grep -q "libgui_shim.so" "${2}" || "${PATCHELF_0_18}" --add-needed "libgui_shim.so" "${2}"
             ;;
         system_ext/lib64/libwfdnative.so)
             [ "$2" = "" ] && return 0
-            grep -q "libinput_shim.so" "${2}" || "${PATCHELF}" --add-needed "libinput_shim.so" "${2}"
+            grep -q "libinput_shim.so" "${2}" || "${PATCHELF_0_18}" --add-needed "libinput_shim.so" "${2}"
             ;;
         system_ext/lib/libwfdservice.so)
             [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
+            "${PATCHELF_0_18}" --replace-needed "android.media.audio.common.types-V2-cpp.so" "android.media.audio.common.types-V3-cpp.so" "${2}"
             ;;
         *)
             return 1
