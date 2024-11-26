ca42e9bda91fc4219860dacdd4e2b844ad9e156e
Nolen Johnson <johnsonnolen@gmail.com>
2024-11-26 15:51:06 -0500

    pro1x: Shim libwvdrmengine with libcrypto_shim as well
    * libwvhidl's patch was resolving the symbol, but lets be proper.
    
    Change-Id: Id30791bdacb0be7066618a7cb8f24eb8e9349281

--- extract-files.sh
+++ extract-files.sh
@@ -73,7 +73,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed libcamera_metadata_shim.so "${2}"
             ;;
-        vendor/lib64/libwvhidl.so)
+        vendor/lib64/libwvhidl.so | vendor/lib64/mediadrm/libwvdrmengine.so)
             [ "$2" = "" ] && return 0
             grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
