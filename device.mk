c1b59467932d62f1002c2a4a31b3c143b600729a
Michael Bestas <mkbestas@lineageos.org>
2025-01-01 03:50:53 +0200

    pro1x: Update display flags & packages
    Required after display HAL blueprint conversion
    
    Change-Id: Ifc4e10703d3424607b9dc8bcc8e417d1c88cc2f3

--- device.mk
+++ device.mk
@@ -139,7 +139,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
-    gralloc.bengal \
     libdisplayconfig.qti \
     libdisplayconfig.system.qti \
     libqdMetaData \
