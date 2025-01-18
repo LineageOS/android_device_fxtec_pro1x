69fbdab309b4b4113e7c0ce284f4d514618f0cac
LuK1337 <priv.luk@gmail.com>
2025-01-18 18:33:32 +0100

    pro1x: Move WfdCommon boot jar declaration to vendor
    Change-Id: I8fa2c0f64692873b432cc252ec88b388a32023a1

--- device.mk
+++ device.mk
@@ -397,10 +397,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.wifi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.wifi.xml \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
-# WiFi Display
-PRODUCT_BOOT_JARS += \
-    WfdCommon
-
 # WiFi Firmware Symlinks
 PRODUCT_PACKAGES += \
     firmware_wlan_mac.bin_symlink \
