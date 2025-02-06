437bda622db167664344efe05365b42b4e5859c2
Michael Bestas <mkbestas@lineageos.org>
2025-02-07 00:36:31 +0200

    pro1x: Don't use BSG framework for gpt-utils
    Change-Id: Ia6e5d651560c8ae5186a48bfbb0e30b864150564

--- device.mk
+++ device.mk
@@ -100,6 +100,8 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.2-impl-qti.recovery \
     android.hardware.boot@1.2-service
 
+$(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
+
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
