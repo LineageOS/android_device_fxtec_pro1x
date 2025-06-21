001a0244511f9ecf6c9aeeb5b960fdb123718b67
basamaryan <basam.aryan@gmail.com>
2025-06-21 12:59:43 -0400

    pro1x: Shim libwfdservice with libaudioclient_shim
    Change-Id: Idd6603ad16926226f22423a2bba2075d430015d5

--- extract-files.py
+++ extract-files.py
@@ -64,6 +64,7 @@ blob_fixups: blob_fixups_user_type = {
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
     'system_ext/lib/libwfdservice.so': blob_fixup()
+        .add_needed('libaudioclient_shim.so')
         .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
 }  # fmt: skip
 
