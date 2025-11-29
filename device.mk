6f6a7f0a2732f3cb208d888432a21c985f6da7c2
Bruno Martins <bgcngm@gmail.com>
2025-11-29 23:31:33 +0000

    pro1x: devicesettings: Add keymaps as required packages
    Change-Id: I72d87be310fbdf135be98311fdb37c9db594612a

--- device.mk
+++ device.mk
@@ -198,11 +198,7 @@ PRODUCT_PACKAGES += \
 # Keyboard
 PRODUCT_PACKAGES += \
     init.keyboard.rc \
-    init.set_layout.sh \
-    QWERTY_keymap \
-    QWERTZ_keymap \
-    AZERTY_keymap \
-    Scandic_keymap
+    init.set_layout.sh
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/idc/Builtin_Keyboard.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/Builtin_Keyboard.idc
