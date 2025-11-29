00335ec781105e61fd117f136a23b5a9c2ffd786
Bruno Martins <bgcngm@gmail.com>
2025-11-29 23:17:36 +0000

    pro1x: devicesettings: Combine with AsantiKeypad
    Change-Id: Ic3c7decece71260fd904f854157129d0b582caa3

--- devicesettings/src/org/lineageos/settings/device/keyboard/InputDeviceReceiver.java
+++ devicesettings/src/org/lineageos/settings/device/keyboard/InputDeviceReceiver.java
@@ -14,7 +14,7 @@
 * limitations under the License.
 */
 
-package org.lineageos.asantikeypad;
+package org.lineageos.settings.device.keyboard;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
