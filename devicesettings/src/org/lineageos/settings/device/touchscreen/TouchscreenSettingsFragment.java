2ddd4ac1f3341558190376d168242ac25ff35b86
Bruno Martins <bgcngm@gmail.com>
2025-12-02 10:59:54 +0000

    pro1x: devicesettings: Modernize touchscreen settings
    Replace usage of custom SeekBarPreference with a SliderPreference
    to go with the new M3E look.
    
    Change-Id: I88bea85469145db238d3c106d8024c50462f3318

--- devicesettings/src/org/lineageos/settings/device/touchscreen/TouchscreenSettingsFragment.java
+++ devicesettings/src/org/lineageos/settings/device/touchscreen/TouchscreenSettingsFragment.java
@@ -12,24 +12,27 @@ import android.os.Bundle;
 import androidx.preference.PreferenceManager;
 
 import com.android.settingslib.widget.SettingsBasePreferenceFragment;
+import com.android.settingslib.widget.SliderPreference;
 
 import org.lineageos.internal.util.FileUtils;
 import org.lineageos.settings.device.R;
-import org.lineageos.settings.device.widget.SeekBarPreference;
 
 public class TouchscreenSettingsFragment extends SettingsBasePreferenceFragment
         implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     private static final String TAG = TouchscreenSettingsFragment.class.getSimpleName();
 
-    private SeekBarPreference mMarginSeekBar;
+    private SliderPreference mMarginSlider;
     private SharedPreferences mPrefs;
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         setPreferencesFromResource(R.xml.touchscreen_panel, rootKey);
 
-        mMarginSeekBar = findPreference(Constants.TOUCHSCREEN_MARGIN_KEY);
+        mMarginSlider = findPreference(Constants.TOUCHSCREEN_MARGIN_KEY);
+        mMarginSlider.setSliderIncrement(1);
+        mMarginSlider.setTickVisible(true);
+        mMarginSlider.setUpdatesContinuously(true);
 
         mPrefs = PreferenceManager.getDefaultSharedPreferences(getContext());
         doUpdateMarginPreference();
@@ -61,6 +64,6 @@ public class TouchscreenSettingsFragment extends SettingsBasePreferenceFragment
                         context.getResources().getInteger(R.integer.touchscreen_margin_default));
 
         FileUtils.writeLine(Constants.TOUCHSCREEN_MARGIN_SYS_FILE, Integer.toString(margin));
-        mMarginSeekBar.setSummary(getString(R.string.touchscreen_margin_summary, margin));
+        mMarginSlider.setSummary(getString(R.string.touchscreen_margin_summary, margin));
     }
 }
