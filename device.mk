c2823809ec685347d8cd9713b8b89afc580c6130
Nolen Johnson <johnsonnolen@gmail.com>
2026-03-23 15:07:27 -0400

    pro1x: Convert all remaining overlay-lineage entries to RRO
    Change-Id: I607dd4e93a9837f0e3666d04208ae5430db5f8c5

--- device.mk
+++ device.mk
@@ -255,15 +255,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.android.nfc_extras.xml
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigOverlay \
     FrameworksOverlay \
     Launcher3DeviceOverlay \
     LineageSDKResCommon \
+    LineageSettingsProviderOverlay \
     SettingsOverlay \
     SettingsProviderOverlay \
     SystemUIOverlay \
