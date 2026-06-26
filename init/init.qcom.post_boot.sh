d79603362e2d8abdc00c8bbfdce7079c86084d72
Nolen Johnson <johnsonnolen@gmail.com>
2026-06-25 23:15:18 -0400

    pro1x: init: Remove vendor_init writes to system_data_file
    * Not needed, the file is not there.
    
    Change-Id: Ibf77e386e3527c867fb3b1b2fc05f3c646ecbdfe

--- init/init.qcom.post_boot.sh
+++ init/init.qcom.post_boot.sh
@@ -6124,7 +6124,6 @@ case "$target" in
         setprop vendor.post_boot.parsed 1
     ;;
     "apq8084")
-        rm /data/system/perfd/default_values
         start mpdecision
         echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
         echo 512 > /sys/block/sda/bdi/read_ahead_kb
