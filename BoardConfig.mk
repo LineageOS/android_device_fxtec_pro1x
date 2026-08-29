e0a8a139db9ca0fcbe43d47dae964e1fa68094fa
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 09:03:01 +0300

    pro1x: Drop no longer used TARGET_KERNEL_NO_GCC
    See: I419ffd8c4f055ca29a6554983ed44aaaab54a666
    Change-Id: I9ce189a991bddd4ec9e42f6853fe5fcd12b51038

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,7 +79,6 @@ BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
-TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/fxtec/sm6115
 TARGET_KERNEL_CONFIG := vendor/bengal-perf_defconfig vendor/ext_config/pro1x.config
 
