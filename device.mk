f6900a0c2478bbf31958871b41bc8393c3670446
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 10:19:42 +0300

    pro1x: Drop UFFD flags
    See: Ibb2c5e9191744f6e15f6bced337ba7d0ed9681ca
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61

--- device.mk
+++ device.mk
@@ -211,10 +211,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.light-service.lineage
 
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := true
-OVERRIDE_ENABLE_UFFD_GC := true
-
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
